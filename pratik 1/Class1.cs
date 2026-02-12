89ab39ed4b6688cc58a89b156f2ea7f69116c48c
Burak Günay <burakgny123@gmail.com>
2026-02-12 15:33:30 +0300

    Update Class1.cs

--- pratik 1/Class1.cs
+++ pratik 1/Class1.cs
@@ -10,14 +10,15 @@ namespace pratik_1
     {
         public string Ad;
         public string soyad;
-        public string sehir;
+        public string sehir; //public olarak tanımladığımız değişkenler sınıfın her yerinden erişilebilir oluyor
 
-        public ogrenci1(string ad, string soyad, string sehir)
+        public ogrenci1(string ad, string soyad, string sehir) //constructor oluşturduk ve parametre olarak ad, soyad ve şehir aldık
         {
             Ad = ad;
             this.soyad = soyad;
-            this.sehir = sehir;
+            this.sehir = sehir;//constructor içinde this anahtar kelimesiyle sınıfın değişkenlerine parametre olarak aldığımız değerleri atıyoruz
         }
     }
     }
+
 
