b2353471eb72868002c9542d244d2f0c026105fe
Burak Günay <burakgny123@gmail.com>
2026-02-12 14:51:47 +0300

    Add files via upload

---
+++ pratik 1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratik_1
+{
+    internal class ogrenci1
+    {
+        public string Ad;
+        public string soyad;
+        public string sehir;
+
+        public ogrenci1(string ad, string soyad, string sehir)
+        {
+            Ad = ad;
+            this.soyad = soyad;
+            this.sehir = sehir;
+        }
+    }
+    }
+
---
+++ pratik 1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pratik_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name=textBox1.Text;
+            string surname=textBox2.Text;
+            string city=textBox3.Text;
+
+            ogrenci1 ogr = new ogrenci1(name,surname,city);
+
+            label7.Text = ogr.Ad;
+            label8.Text = ogr.soyad;
+            label9.Text = ogr.sehir;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
